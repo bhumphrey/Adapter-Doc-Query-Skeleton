75dc10199c3f13c6e06ae22c0cfb495809c9036b
Brian Humphrey <bhumphrey@5amsolutions.com>
2014-02-07 08:31:47 -0500

    correct mock doc repo for test

--- src/test/java/com.fiveamsolutions.connect.adapter.docquery/AdapterDocQueryTest.java
+++ src/test/java/com.fiveamsolutions.connect.adapter.docquery/AdapterDocQueryTest.java
@@ -6,26 +6,18 @@ import oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
-import org.mockito.MockitoAnnotations.Mock;
-
 import javax.inject.Inject;
 
 public class AdapterDocQueryTest extends AbstractAdapterDocQueryTest {
 
 
-    @Mock
-    DocRepository mockRepository;
-
-    @InjectMocks
     @Inject
-    AdapterDocQueryPortTypeImpl webservice;
+    DocRepository mockRepository;
 
 
     @Before
     public void initMocks() {
-        MockitoAnnotations.initMocks(this);
+
     }
 
 
